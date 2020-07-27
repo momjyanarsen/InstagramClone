86026bdf6b6d13a68e81c074dbc5dbb1494ccfbc
momjyan <momjyan@synopsys.com>
2020-07-27 23:43:44 +0400

    This commit creates SectionsPagerAdapter Tabs
    This commit creates tabs in Home, (BottomNavigationView)
    Tabs->Camera, Message ...

--- app/src/main/java/com/example/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/instagramclone/Home/HomeActivity.java
@@ -1,14 +1,17 @@
 package com.example.instagramclone.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewParent;
 
 import com.example.instagramclone.R;
+import com.google.android.material.tabs.TabLayout;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import com.example.instagramclone.Utils.BottomNavigationViewHelper;
@@ -25,6 +28,28 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Log.i("INFO", "onCreate: starting.");
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Responsible for adding tree tabs: Camera, Home, Messages
+     */
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new CameraFragment());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new MessagesFragment());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon((R.drawable.ic_arrow));
     }
 
     /**
---
+++ app/src/main/java/com/example/instagramclone/Home/SectionsPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.instagramclone.Home;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class taht stores fragments for tabs
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public static final String Tag = "SectionPagerAdapter";
+
+    public static List<Fragment> mFragmentList = new ArrayList<>();
+
+    public SectionsPagerAdapter (FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment (Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+}
