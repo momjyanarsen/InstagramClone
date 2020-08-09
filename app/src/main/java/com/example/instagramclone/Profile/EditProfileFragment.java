6b65b20d956253fff0fa782d4af3403f95dc034a
momjyan <momjyanarsen@gmail.com>
2020-08-10 01:15:35 +0400

    This commit creates Universal Image Loader configuration
    This commit creates UniversalImageLoader class in /Utils
    which can setup and config UniversalImageLoader

--- app/src/main/java/com/example/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/Profile/EditProfileFragment.java
@@ -1,25 +1,46 @@
 package com.example.instagramclone.Profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.instagramclone.R;
+import com.example.instagramclone.Utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
 
     private static final String TAG = "EditProfileFragment";
 
+    private ImageView mProfilePhoto;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
 
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage() {
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "https://mojautomobil.net/wp-content/uploads/2017/12/ANDROID.png";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "");
     }
 }
