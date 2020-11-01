c34b4433052c0a03e5064283ce171d7c782fc414
momjyan <momjyanarsen@gmail.com>
2020-11-01 23:06:07 +0400

    Creates RegisterActivity Widgets

--- app/src/main/java/com/example/instagramclone/Login/RegisterActivity.java
+++ app/src/main/java/com/example/instagramclone/Login/RegisterActivity.java
@@ -1,21 +1,109 @@
 package com.example.instagramclone.Login;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.instagramclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
+    private Context mContext;
+    private String email, username, password;
+    private EditText mEmail, mPassword, mUsername;
+    private TextView loadingPleaseWait;
+    private Button btnRegister;
+    private ProgressBar mProgressBar;
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Log.d(TAG, "onCreate: started");
+
+        initWidgets();
+        setupFirebaseAuth();
+    }
+
+    /**
+     *  Initialize the activity widgets
+     */
+
+    private void initWidgets() {
+        Log.d(TAG, "initWidgets: Initializing Widgets");
+        mEmail = (EditText) findViewById(R.id.input_email);
+        mUsername = (EditText) findViewById(R.id.input_username);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        loadingPleaseWait = (TextView) findViewById(R.id.loadingPleaseWait);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mContext = RegisterActivity.this;
+        mProgressBar.setVisibility(View.GONE);
+        loadingPleaseWait.setVisibility(View.GONE);
+    }
+
+    private boolean isStringNull(String string) {
+        Log.d(TAG, "isStringNull: checking string if null");
+        if(string.equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    --------------------------------------- Firebase ---------------------------------------------
+     */
+
+    /**
+     *  Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged_in" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
